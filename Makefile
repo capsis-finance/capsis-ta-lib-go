c2b027c31be11977cf3f40fa29ec84b2211ae391
capsian <selim.lakhdar@gmail.com>
2024-08-30 00:41:47 +0200

    add export test_data

--- Makefile
+++ Makefile
@@ -16,9 +16,12 @@ install_venv: requirements.txt
 # Script
 
 get_test_data_ETHUSDT_1m:
-	$(PY_CMD) script/test_data.py -s "ETHUSDT" -i "1m"
+	$(PY_CMD) script/test_data.py -s "ETHUSDT" -i "1m" -l 100 -e
 
 # Test
 
 test_csv_reader:
 	go test -v ./internal/csv/reader_test.go
+
+test_ichimoku:
+	go test -v ./pkg/ichimoku/ichimoku_test.go
--- script/test_data.py
+++ script/test_data.py
@@ -10,8 +10,8 @@ kline_cols = ['t', 'o', 'h', 'l', 'c', 'vb', 'close_time', 'vq', 'n', 'vbt', 'vq
 kline_cols_drop = ['close_time', 'ignore', 'vbt', 'vqt', ]
 
 
-def fetch_data(_symbol, _interval, _output_csv):
-    _data = requests.get('https://api.binance.com/api/v3/klines?symbol=' + _symbol + '&interval=' + _interval).json()
+def fetch_data(_symbol, _interval, _limit, _output_csv):
+    _data = requests.get('https://api.binance.com/api/v3/klines?symbol=' + _symbol + '&interval=' + _interval + '&limit=' + str(_limit)).json()
     _df = pd.DataFrame(_data, columns=kline_cols).drop(kline_cols_drop, axis=1)
     _df.to_csv(_output_csv, index=False)
     return _df
@@ -26,7 +26,7 @@ def get_indicators(_df_kline, _output_csv):
     _df_indicators['t_copy'] = _df_indicators['t'].copy()
 
     # set time index
-    _df_indicators['t'] = pd.to_datetime(_df_indicators['t'], unit='ms')
+    _df_indicators['t'] = pd.to_datetime(_df_indicators['t'], unit='ms') + pd.Timedelta(hours=2)
     _df_indicators = _df_indicators.set_index('t')
 
     # reset time column
@@ -67,20 +67,29 @@ def get_indicators(_df_kline, _output_csv):
     return _df_indicators
 
 
-def main(_symbol, _interval, _kline_limit, _run_output_dir):
+def main(_symbol, _interval, _kline_limit, _run_output_dir, _export):
     _base_file_name = _run_output_dir + _symbol + '_' + _interval
     print("_base_file_name:", _base_file_name)
 
     # fetch kline & save
     kline_output = _base_file_name + '_kline.csv'
-    df_kline = fetch_data(_symbol, _interval, kline_output)
+    df_kline = fetch_data(_symbol, _interval, _kline_limit, kline_output)
     print("kline_output:", kline_output)
+    if _export:
+        export_kline_path = _run_output_dir + "../test/test_kline.csv"
+        df_kline.to_csv(export_kline_path, index=False)
+        print("export_kline_path:", export_kline_path)
 
     # add indicators
     indic_output = _base_file_name + '_indicator.csv'
     df_indic = get_indicators(df_kline, indic_output)
     print("indic_output:", indic_output, "\n")
     print("df_indic:", df_indic)
+
+    if _export:
+        export_indic_path = _run_output_dir + "../test/test_indicator.csv"
+        df_indic.to_csv(export_indic_path, index_label='t')
+        print("export_indic_path:", export_indic_path)
 
 
 def parse_args():
@@ -89,6 +98,7 @@ def parse_args():
     _parser.add_argument("-s", "--symbol", type=str, help="symbol", required=True)
     _parser.add_argument("-i", "--interval", type=str, help="interval", required=True)
     _parser.add_argument("-l", "--limit", type=int, help="limit", required=False)
+    _parser.add_argument("-e", "--export", action='store_true', help="export as test (save to test_data/test)", required=False)
     _args = _parser.parse_args()
     return _args
 
@@ -97,8 +107,11 @@ if __name__ == '__main__':
     # parse args
     args = parse_args()
 
+    if args.limit is None:
+        args.limit = 500
+
     # init output name
-    _base_output_name = args.symbol + '_' + args.interval
+    _base_output_name = args.symbol + '_' + args.interval + "_limit" + str(args.limit)
 
     # init output dir
     run_time = datetime.datetime.now().strftime("%Y_%m_%d_%H:%M:%S")
@@ -106,7 +119,4 @@ if __name__ == '__main__':
     print("run_output_dir:", run_output_dir)
     os.mkdir(run_output_dir)
 
-    if args.limit is None:
-        args.limit = 500
-
-    main(args.symbol, args.interval, args.limit, run_output_dir)
+    main(args.symbol, args.interval, args.limit, run_output_dir, args.export)
